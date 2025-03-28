7c01b6cae91969e8fd4b94948cd850a10c19c315
aidenaltman <aiden.altman@gmail.com>
2025-03-28 13:52:29 -0400

    feat: Working login -> home page

---
+++ frontend/src/HomePage.jsx
@@ -0,0 +1,13 @@
+// src/Dashboard.jsx
+import React from "react";
+
+function Dashboard() {
+    return (
+        <div>
+            <h1>Testing</h1>
+            <p>Testing</p>
+        </div>
+    );
+}
+
+export default Dashboard;
