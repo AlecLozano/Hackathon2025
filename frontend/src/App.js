ed58b907c0ec7599e6ab8b1feda72c391e8dbd37
aidenaltman <aiden.altman@gmail.com>
2025-03-28 14:12:02 -0400

    test msg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-import { ChakraProvider } from '@chakra-ui/react';
-import Login from './Login.jsx'; // Capitalize Login
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
+import Login from "./Login"; // Import Login Component
 
 function App() {
-  return (
-    <Login />
-  );
-}
+    const [registered, setRegistered] = useState(false); // State to track registration
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
+    const onSubmit = (data) => {
+        localStorage.setItem(data.email, JSON.stringify(data)); // Store user details
+        console.log("User Registered:", data);
+        setRegistered(true); // Switch to Login page
+
+        
+    };
+
+    return (
+        <>
+            {registered ? (
+                <Login />
+            ) : (
+                <>
+                    <p className="title">Registration Form</p>
+                    <form className="App" onSubmit={handleSubmit(onSubmit)}>
+                        <input type="text" {...register("name", { required: true })} placeholder="Name" />
+                        <input type="email" {...register("email", { required: true })} placeholder="Email" />
+                        {errors.email && <span style={{ color: "red" }}>*Email* is mandatory</span>}
+                        <input type="password" {...register("password", { required: true })} placeholder="Password" />
+                        <input type="submit" value="Register" style={{ backgroundColor: "#a1eafb" }} />
+                    </form>
+                </>
+            )}
+        </>
+    );
+}
 export default App;
